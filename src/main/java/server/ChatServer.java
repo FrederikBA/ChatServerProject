e60caa63393946ce47c184ac6125f6d9c29927c5
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-03-08 10:36:38 +0100

    test

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -10,25 +10,20 @@ public class ChatServer {
 
     //Call server with arguments like this: 0.0.0.0 8088 logfile.log
     public static void main(String[] args) throws UnknownHostException {
-        String ip ="localhost";
+        String ip = "localhost";
         int port = 8088;
         String logFile = "log.txt";  //Do we need this
-
         try {
             if (args.length == 3) {
                 ip = args[0];
                 port = Integer.parseInt(args[1]);
                 logFile = args[2];
-            }
-            else {
+            } else {
                 throw new IllegalArgumentException("Server not provided with the right arguments");
             }
         } catch (NumberFormatException ne) {
             System.out.println("Illegal inputs provided when starting the server!");
             return;
         }
-
     }
-
-
 }
