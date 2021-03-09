8ab2ef09bf7d1adad00d5cf06615b4fec5cf0c4b
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-03-09 14:14:16 +0100

    bug fix

---
+++ src/main/java/Client/Client.java
@@ -0,0 +1,39 @@
+package Client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * CREATED BY Janus @ 2021-03-08 - 10:15
+ **/
+public class Client {
+
+    Socket socket;
+    PrintWriter pw;
+    Scanner sc;
+
+    public void connect(String address, int port) throws IOException {
+        socket = new Socket(address, port);
+        pw = new PrintWriter(socket.getOutputStream(), true);
+        sc = new Scanner(socket.getInputStream());
+        System.out.println(sc.nextLine());
+        Scanner keyboard = new Scanner(System.in);
+        boolean keepRunning = true;
+        while (keepRunning) {
+            String messageToSend = keyboard.nextLine();
+            pw.println(messageToSend);
+            System.out.println(sc.nextLine());
+            if (messageToSend.equals("stop")) {
+                keepRunning = false;
+            }
+        }
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Client().connect("localhost", 5555);
+    }
+}
+
